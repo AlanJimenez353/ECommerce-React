6253e7b296b8013606ba77ccb0bff9f9dde8c9db
Alan Jimenez <ajimenez@technisys.com>
2021-12-03 01:11:01 -0300

    Se agrego setLoading

--- src/components/ItemListConteiner/ItemListConteiner.js
+++ src/components/ItemListConteiner/ItemListConteiner.js
@@ -5,15 +5,16 @@ import { pedirDatos } from "../utils/pedirDatos";
 
 
 
-export const ItemListConteiner = ({greeting}) =>{
+export const ItemListConteiner = () =>{
 
     ///COMIENZO CON LAS FECHAS VACIAS
 const [fechas,setFechas]= useState([])
-
+const[loading,setLoading]=useState(false)
 
 //REALIZO RENDERIZACION Y CUANDO ACTUALIZA YA TENGO MIS FECHAS CARGADAS
 useEffect(()=>{
 
+    setLoading(true)
     pedirDatos()
         .then((resp)=>{
             setFechas(resp)
@@ -24,13 +25,19 @@ useEffect(()=>{
         })
         .finally(()=>{
             console.log("Peticion finalizada")
+            setLoading(false)
         })
 },[])
 
 // UNA VEZ CON LAS FECHAS CARGADAS LLAMO A  FUNCION QUE CREA TABLA DE FECHAS
+// Utilizo el set loading para que los componentes del ItemList no se renderizen antes de estar cargados
     return(
-        <div>
-            <FechasList Fechas= {Fechas}/>
+        <div> 
+            {
+                loading
+                ? <h2>Cargando...</h2>
+                : <FechasList Fechas= {Fechas}/>
+            }
         </div>
     )
 
