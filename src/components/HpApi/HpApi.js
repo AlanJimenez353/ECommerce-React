4bbb3ee3eb3577d79bc8ede8e4f6cf598f229561
Alan Jimenez <ajimenez@technisys.com>
2021-12-04 19:20:38 -0300

    TerceraEntrega completa

--- src/components/HpApi/HpApi.js
+++ src/components/HpApi/HpApi.js
@@ -4,15 +4,24 @@ import { useFetch } from "../Hooks/useFetch";
 
 export const HpApi = () => {
 
-    const {data} = useFetch("http://hp-api.herokuapp.com/api/characters/students")
+    const [id,setId]= useState(0)
+    const {data} = useFetch(`http://hp-api.herokuapp.com/api/characters/students`,[])
     console.log(data)
+    
+    const handleSiguiente= () =>{
+        setId(id+1)
+    }
+    
+    
+    
     return (
         <div className="container my-5">
                 {data && 
                 <>
-                <h2>Su personaje es: {data[0].name}</h2>
-                <img src={data[0].image} alt={data[0].name}></img>
-                <p>Casa: {data[0].house}</p>
+                <h2>{data[id].name}</h2>
+                <img src={data[id].image} alt={data[id].name}></img>
+                <p>Casa: {data[id].house}</p>
+                <button className="btn btn-dark" onClick={handleSiguiente}>Ver siguiente Personaje</button>
         
                 </>
                 }
