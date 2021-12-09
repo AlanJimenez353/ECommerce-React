3f4328c0eebaca3950e9d478514ddd8ef00b60fe
Alan Jimenez <ajimenez@technisys.com>
2021-12-08 23:59:55 -0300

    +Botones Carrito

--- src/components/cartView/CartView.js
+++ src/components/cartView/CartView.js
@@ -1,9 +1,11 @@
 import React, { useContext } from "react";
 import { cartContext } from "../context/cartContext";
+import {BsTrashFill} from 'react-icons/bs'
+import {Link} from 'react-router-dom'
 
 export const CartView = () => {
 
-    const {cart}=useContext(cartContext)
+    const {cart,vaciarCarrito,totalCompra,removerDelCarrito}=useContext(cartContext)
 
 
     return(
@@ -11,19 +13,36 @@ export const CartView = () => {
         <div className="conteiner my-5">
             <h2>Cart View</h2>
             <hr></hr>
+            {   cart.lenght === 0
+                ?  <>
+                    <h2>No tienes elementos en tu carrito</h2>
+                    <Link to="/">Inicio</Link>
+                   </> 
+                :
+                    <>
+                    <h2>Tu Compra</h2>
+                { cart.map( (el) => (
+                    <div key={el.id}>
+                        <h3>Evento :  {el.artista}</h3>
+                        <p>Precio unitario:  {el.precio}</p>
+                        <p>Cantidad Entradas:  {el.cantidad}</p>
+                        <button className="btn btn-dark"
+                            //Funcion anonima
+                            onClick={ () =>{removerDelCarrito(el.id)} }>
+                            <BsTrashFill/>
+                        </button>
+                        <hr></hr>
+                    </div>
+                 ))
+                }
 
-          {
-            cart.map( (el) => (
-                <div key={el.id}>
-                    <h3>{el.artista}</h3>
-                    <p>{el.precio}</p>
-                    <p>{el.cantidad}</p>
+                    <h4>Total:{totalCompra()}</h4>
                     <hr></hr>
-
-                </div>
-             ))
-        }
+                    <button className="btn btn-danger" onClick={vaciarCarrito}>Vaciar carrito</button>
+                    <Link to="/checkout " className="btn btn-success mx-3">Terminar compra</Link>
+                    </>
+            }
         </div>
-
+    
     )
 }
--- src/components/context/cartContext.js
+++ src/components/context/cartContext.js
@@ -8,27 +8,41 @@ export const CartProvider= ({children}) =>{
     const[cart,setCart]=useState([])
     console.log(cart)
     
+
+//Funciones
     const agregarAlCarrito = (fecha) =>{
         setCart( [...cart, fecha])
-    }
-  
-  //Some funciona como el find pero retorna true or false
+    }  
+    //Some funciona como el find pero retorna true or false
     const isInCart=(id)=>{
           return cart.some((e) => e.id === id)
       }
-
     //CartWidgetCounter
     const totalCantidad= () =>{
         return cart.reduce((acc,el) => acc + el.cantidad,0 )
 
     }
+    const vaciarCarrito=() =>{
+        setCart([])
+    }
+    const totalCompra= () => {
+        return cart.reduce( (acc,el)=> acc +el.cantidad * el.precio,0)
+    }
+    const removerDelCarrito=(id)=>{
+        //Retorna array con todos los elementos cuyo id que se le pasa por parametro
+        setCart(cart.filter((el) => el.id !== id))
+    }
 
+    
     return(
         <cartContext.Provider value={ {
             cart,
             agregarAlCarrito, 
             isInCart,
-            totalCantidad
+            totalCantidad,
+            vaciarCarrito,
+            totalCompra,
+            removerDelCarrito
           } }>
         {/* Si o si tengo que devolver los children que capturo en App.js}*/}
             {children}
